bec776b0a36e427e5430e52e671dc2d8307408a1
Amirhossein <amir78729@aut.ac.ir>
2019-07-24 21:07:34 +0430

    winning and losing added:)

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static boolean isDead;
     public static char[] guess;
     public static char guessedChar;
+    public static int wrongAnswers;
+    public static int correctAnswers;
 
     public static void main(String[] args) {
 //        JFrame frame = new MyFrame();
@@ -15,6 +17,8 @@ public class Main {
         myWord = scanner.nextLine();
         isDead = false;
         guess= new char[myWord.length()];
+        wrongAnswers = 0;
+        correctAnswers = 0;
 
         for (int i = 0; i < myWord.length(); i++)
             guess[i] = '_' ;
@@ -22,7 +26,34 @@ public class Main {
         while(!isDead){
             System.out.println("USER 2\nGuess a character:");
             guessedChar = scanner.next().charAt(0);
-            System.out.println(contains(myWord , guessedChar));
+            //if user's guess was true...
+            if(contains(myWord , guessedChar)){
+                boolean flag = false;
+                for(int i = 0; i < myWord.length(); i++) {
+                    if (myWord.charAt(i) == guessedChar) {
+                        guess[i] = guessedChar;
+                        correctAnswers++;
+                        if (correctAnswers >= myWord.length()) {
+                            System.out.println("congratulations!!");
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+                if (flag)
+                    break;
+            }
+            //if user's guess was false...
+            else{
+                wrongAnswers++;
+                if(wrongAnswers >= 6){
+                    System.out.println("YOU LOSE!");
+                    break;
+                }
+
+            }
+            printTheGuess();
+
         }
     }
 
@@ -33,6 +64,9 @@ public class Main {
     public static void printTheGuess(){
         for (int i = 0; i < myWord.length(); i++)
             System.out.print(guess[i] + " ");
+        System.out.println("");
+        System.out.println("correct answers: " + correctAnswers + "/" + myWord.length());
+        System.out.println("wrong answers: " + wrongAnswers + "/6");
     }
 
 
