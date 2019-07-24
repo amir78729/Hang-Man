68b8daad0e478bb6c36b117f6c20f9603b8a44e3
Amirhossein <amir78729@aut.ac.ir>
2019-07-24 20:20:45 +0430

    files added:)

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Main {
+    public static String myWord;
+    public static boolean isDead;
+    public static char[] guess;
+    public static void main(String[] args) {
+        JFrame frame = new MyFrame();
+        myWord = "hangman";
+        isDead = false;
+        guess= new char[myWord.length()];
+        for (int i = 0; i < myWord.length(); i++)
+            guess[i] = '_' ;
+        printTheGuess();
+    }
+
+
+
+
+
+    public static void printTheGuess(){
+        for (int i = 0; i < myWord.length(); i++)
+            System.out.print(guess[i] + " ");
+    }
+}
